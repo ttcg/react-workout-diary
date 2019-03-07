cac6f64de24bc9f1b8f10d6bf303963effa48093
Thet Wai <thetwai2000@gmail.com>
2019-03-07 21:57:18 +0000

    replaced Svg spinner with text spinner

--- src/components/common/loadingSpinner.js
+++ src/components/common/loadingSpinner.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Delay from 'react-delay';
-import ReactLoading from 'react-loading'
 import './loadingSpinner.css';
 
 const loadingSpinner = () => (
     <Delay wait={300}>
         <div className="modal-loading">
             <div className="modal-loading-content">
-                <ReactLoading type="spinningBubbles" color="#000" height={'100%'} width={'100%'} />
+                Loading...
             </div>
         </div>
     </Delay>
