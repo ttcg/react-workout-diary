3862befb3c628093100124f7aee5bf304610f039
Thet Wai <thetwai2000@gmail.com>
2019-03-31 20:59:59 +0100

    added spinning icon in Spinner

--- src/components/common/loadingSpinner.js
+++ src/components/common/loadingSpinner.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Delay from 'react-delay';
 import './loadingSpinner.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const loadingSpinner = () => (
     <Delay wait={300}>
         <div className="modal-loading">
-            <div className="modal-loading-content">
+            <div className="modal-loading-content text-center">
+                <FontAwesomeIcon
+                    icon='spinner'
+                    size="2x"
+                    pulse
+                    fixedWidth />
                 Loading...
             </div>
         </div>
