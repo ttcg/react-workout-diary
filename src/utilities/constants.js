260a88e0d88c26f86405bcb55416142f94c6bf03
Thet Wai <thetwai2000@gmail.com>
2019-04-14 20:04:28 +0100

    new files added

---
+++ src/utilities/constants.js
@@ -0,0 +1,15 @@
+const Routes = {
+    Home: '/home',
+    Workouts: '/workouts',
+    WorkoutsApi: '/workoutsapi',
+    WorkoutsReact: '/workoutsreact',
+    Unauthorised: '/unauthorised',
+    About: '/about',
+    SecurePage: '/securepage'
+}
+
+const Constants = {
+    Routes
+}
+
+export default Constants; 
