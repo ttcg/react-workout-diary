a666d5c49bfd780dee7155555f418665988d206f
Thet Wai <thetwai2000@gmail.com>
2019-06-02 16:32:02 +0100

    Fix the broken tests and refresh the snapshots to adapt new React and Test package versions

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
 import es6Promise from 'es6-promise';
 import es6ObjectAssign from 'es6-object-assign';
 import { ToastContainer } from 'react-toastify';
@@ -24,31 +23,22 @@ fontAwesome.setup();
 es6Promise.polyfill();
 es6ObjectAssign.polyfill();
 
-class App extends Component {
-	render() {
-
-		return (
-			<BrowserRouter>
-				<React.Fragment>
-					<Header />
-					<div className="content">
-						<Routes />
-					</div>
-					<ToastContainer
-						autoClose={2000}
-					/>					
-					<Footer />
-					<LoadingSpinner />
-				</React.Fragment>
-			</BrowserRouter>
-		);
-	}
+const App = () => {
+	return (
+		<BrowserRouter>
+			<React.Fragment>
+				<Header />
+				<div className="content">
+					<Routes />
+				</div>
+				<ToastContainer
+					autoClose={2000}
+				/>
+				<Footer />
+				<LoadingSpinner />
+			</React.Fragment>
+		</BrowserRouter>
+	);
 }
 
-const mapStateToProps = (state) => {
-	return {		
-		authentication: state.authentication
-	}
-}
-
-export default connect(mapStateToProps)(App);
+export default App;
