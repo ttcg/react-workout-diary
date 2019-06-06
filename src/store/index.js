e307983b05ead1f12b9cc0f69a345ad3fa7717ee
Thet Wai <thetwai2000@gmail.com>
2019-06-06 21:50:02 +0100

    enable redux-devtools

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import rootReducer from "../reducers/index";
 import thunk from "redux-thunk";
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootSaga from '../sagas';
 
@@ -10,7 +11,10 @@ const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
     rootReducer,
-    applyMiddleware(thunk, logger, sagaMiddleware));
+    composeWithDevTools(
+        applyMiddleware(thunk, logger, sagaMiddleware)
+    )
+);
 export default store;
 
 sagaMiddleware.run(rootSaga);
